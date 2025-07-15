282a655512eb6e701eda4a9a7b1495181e5b8ab6
Sujitha Wijewantha <sujithacrysis@gmail.com>
2025-07-15 20:26:04 +0530

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
@@ -9,9 +9,9 @@ export default defineConfig({
   },
   plugins: [
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart({ target: "vercel", customViteReactPlugin: true }),
     viteReact(),
   ],
-})
+});
